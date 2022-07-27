115cf9926b3a8f9449c68d9729112d13569e38f8
Moe <m.ibrahim0228@gmail.com>
2022-07-27 11:41:57 -0400

    getCallableInteger mehod implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 import java.util.concurrent.Callable;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Integer num = getCallableInteger().call();
+        System.out.println(num);
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
+
+        Callable<Integer> integerCallable = () -> {
+            Scanner scanner = new Scanner(System.in);
+            Integer num = scanner.nextInt();
+            return num;
+        };
+        return integerCallable;
     }
 }
