91e42bfdcb00059e2f2fe7c3a8a8c075b90c7e88
Moe <m.ibrahim0228@gmail.com>
2022-07-27 14:05:50 -0400

    getCallableInteger method fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 public class Main {
     public static void main(String[] args) throws Exception {
         Integer num = getCallableInteger().call();
+        System.out.println(num.getClass());
         System.out.println(num);
     }
     public static Callable<Integer> getCallableInteger() {
